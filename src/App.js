b1efa614b356ac7250ce76ce9a2f22b2627b0537
mandalaad <mandalacs18@gmail.com>
2023-04-19 05:17:50 +0700

    update jumbotron

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './component/Navbar';
 import Footer from './component/footer';
 import Discount from './component/discount'
-
+import Discovery from './component/jumbotron/discovery';
 
 
 function App() {
     return (
       <>
       <Navbar/>
+      <Discovery/>
       <Discount/>
       <Footer/>
       
--- src/component/Jumbotron/Discovery/index.jsx
+++ src/component/Jumbotron/Discovery/index.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import { BsArrowRightCircle } from 'react-icons/bs'
+import imageb1 from '../../../asset/images/b1.jpg'
+import './style.css'
 function Discovery() {
   return (
-    <div>Discovery</div>
+    <div className='hero'>
+        <Container className='mt-5'>
+            <Row>
+                <Col lg={8} md = {12}  className='left-hero'>
+                    <div className='content-box'>
+                        <h1>Exploring Wonderful Indonesia</h1>
+                        <p>
+                            At Wonderful Indonesia, we believe that travel should be an unforgettable experience, and we're dedicated to making sure that every moment you spend with us is one to remember. So why wait? Book your adventure today and discover the magic of Indonesia.
+                        </p>
+                        <Button variant='outline'>Explore Now<i className='px-3'><BsArrowRightCircle/></i></Button>
+                    </div>
+                </Col>
+                <Col lg={4} md={12}>
+                  <div className='banner'>
+                    <img src={imageb1} alt="" />
+                  </div>
+                  <div className='banner1'>
+                    <img src={imageb1} alt="" />
+                  </div>
+                </Col>
+            </Row>
+        </Container>
+    </div>
   )
 }
 
