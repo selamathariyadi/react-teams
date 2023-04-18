860d308aee74a3bbd6637fad64478aa7af4b69dc
selamathariyadi <selamath6@gmail.com>
2023-04-19 03:41:44 +0700

    add changename file at jumbtron

--- src/component/jumbotron/discovery/index.jsx
+++ src/component/jumbotron/discovery/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function index() {
+function Discovery() {
   return (
-    <div>index</div>
+    <div>Discovery</div>
   )
 }
 
-export default index
+export default Discovery
